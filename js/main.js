46d239e8954c651de10de0170b114d98503b96dc
Ivan Diaz <idiaz3@gmu.edu>
2015-02-18 10:16:11 -0500

    Updated side scrolling background

--- js/main.js
+++ js/main.js
@@ -7,19 +7,30 @@ var ground;
 var platform;
 var background;
 var keys = Phaser.Keyboard;
+var jump;
 var map; // Map from the Tiled editor
 var layer; //Layers from the maptiles
 
 function preload() {
     game.load.image('darkbg','assets/background_by_sapphireitrenore.png');
     game.load.image('ground','assets/platform_black.png');
+    game.load.audio('music',['assets/audio/Bonobo-Stay_The_Same_(Instrumental).mp3']);
+    game.load.audio('jump',['assets/audio/mario_jump.mp3']);
     game.load.spritesheet('dude','assets/dude.png',32,48);
 
 }
 
 function create() {
+    
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    background = game.add.tileSprite(0,0,1037,720,'darkbg');
+    //background = game.add.tileSprite(0,0,1037,720,'darkbg');
+    game.world.setBounds(0,0,1037,720);
+    background = game.add.tileSprite(0, 0, game.world.width, game.cache.getImage('darkbg').height,'darkbg');
+    var music = game.add.audio('music');
+    music.volume = 0.3;
+    music.loop = true;
+    music.play();
+    jump = game.add.audio('jump');
     
   
     //Enabling physics on ground and spawning them
@@ -46,27 +57,32 @@ function create() {
 function update() {
     game.physics.arcade.collide(player, platform);
     //game.physics.arcade.collide(player, layer);
-    
     player.body.velocity.x = 0;
+    
     if(game.input.keyboard.isDown(keys.RIGHT))
     {
 	player.body.velocity.x = 175;
 	player.animations.play('right');
+	background.tilePosition.x -= 1;
     }
     else if(game.input.keyboard.isDown(keys.LEFT))
     {
 	player.body.velocity.x = -175;
 	player.animations.play('left');
+	background.tilePosition.x += 1;
     }
     else
     {
 	player.animations.stop();
 	player.frame = 4;
+//	background.tilePosition.x = 0;
     }
     
     //Jumping
     if(game.input.keyboard.isDown(keys.UP) && player.body.touching.down)
     {
+	jump.volume = 0.4;
+	jump.play();
 	player.body.velocity.y = -425;
     }
 }
